c9b9addad46fdb5efc0506ba1b0784bc45620a8d
prabin-acharya@ <acharyaprabin101@gmail.com>
2021-06-02 19:22:44 +0545

    Stack - Array Implementation

--- README.md
+++ README.md
@@ -13,11 +13,11 @@ LinkedList2.cpp     : Inserting a node at nth position of LInked List.
 
 
 LinkedList3.cpp     : Delete a node at nth position in Linked list.
-Dynamic Memory Allocation;stack and queue
+					Dynamic Memory Allocation;stack and queue
 
 
 LinkedList4.cpp     : Reverse a linked List -Iterative method.
-Head as a local variable.Functions with head as arguments and return-type.
+					Head as a local variable.Functions with head as arguments and return-type.
 
 
 LinkedList5.cpp     : Print Linked List using Recursion / Recursive traversal of linked list.
@@ -28,6 +28,9 @@ LinkedList6.cpp     : Reverse a linked list using recursion.
 
 
 DoublyLinkedList    : Implementation of DoublyLinkedList and various operations on it.
+
+
+Stack_array.cpp 	: Stack - Array Implementation.
 
 
 
---
+++ Stack_array.cpp
@@ -0,0 +1,73 @@
+/*Stack : Array Implementation */
+#include<iostream>
+using namespace std;
+
+#define MAX_SIZE  100
+
+class Stack
+{
+	int top;
+public:
+	int A[MAX_SIZE];
+	Stack (){ top = -1;}
+	void Push(int);
+	void Pop();
+	int Top();
+	bool isEmpty();
+	void Print();
+};
+
+void Stack::Push(int x)
+{
+	if(top == MAX_SIZE-1)
+	{
+		cout << "Error : Stack Overflow" << endl;
+		return;
+	}
+	A[++top] = x;
+	//top++;
+	//A[top] = x;
+}
+
+void Stack::Pop()
+{
+	if(top == -1)
+	{
+		cout << "Error : Stack Underflow(No element to pop)" << endl;
+		return;
+	}
+	top--;
+}
+
+int Stack::Top()
+{
+	return A[top];
+}
+
+bool Stack::isEmpty()
+{
+	return (top<0);
+}
+
+void Stack::Print()
+{
+	cout << "Stack : ";
+	for(int i = 0; i <= top; i++)
+	{
+		cout << A[i] << " ";
+	}
+	cout << endl;
+}
+
+
+int main()
+{
+	Stack s;
+	s.Push(2);s.Print();
+	s.Push(4);s.Print();
+	s.Push(6);s.Print();
+	s.Push(8);s.Print();
+	s.Pop();s.Print();
+	s.Pop();s.Print();
+	cout<<s.isEmpty();
+}

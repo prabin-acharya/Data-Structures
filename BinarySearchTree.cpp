84ea557ab85a5728061e8535b4781c69201e0e6a
Prabin Acharya <acharyaprabin101@gmail.com>
2022-02-21 22:35:44 +0545

    Search BST

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -50,8 +50,21 @@ BstNode *Insert(BstNode *root, int data)
 //     }
 // }
 
+bool Search(BstNode *root, int data)
+{
+    if (root == NULL)
+        return false;
+    else if (root->data == data)
+        return true;
+    else if (data <= root->data)
+        return Search(root->left, data);
+    else
+        return Search(root->right, data);
+}
+
 int main()
 {
+
     BstNode *root = NULL; // Creating an empty tree;
     root = Insert(root, 15);
     root = Insert(root, 20);
@@ -65,4 +78,12 @@ int main()
     // Insert(&root, 25);
     // Insert(&root, 8);
     // Insert(&root, 12);
+
+    int number;
+    cout << "Enter number to be searched: ";
+    cin >> number;
+    if (Search(root, number) == true)
+        cout << "Found\n";
+    else
+        cout << "Not Found\n";
 }
